c8b3ead6df3efb3c3b32a77e7e76a8efdcf252ca
linnpehtut <linnpehtut@gmail.com>
2023-12-20 14:57:47 +0630

    rtb subent association

--- main.tf
+++ main.tf
@@ -40,4 +40,7 @@ resource  "aws_route_table" "tf-route-table" {
     }
 }
 
-
+resource "aws_route_table_association" "rtb-subnet-asso" {
+    subnet_id = aws_subnet.tf-subnet-1.id
+    route_table_id = aws_route_table.tf-route-table.id
+}
